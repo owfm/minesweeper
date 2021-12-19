289eab6f42c2072c6d0b2c6f863810150d969525
Oliver Mansell <Oliver.Mansell@StoneX.com>
2021-12-19 21:20:03 +0000

    fix: fanout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
 const GRID_SIZE_X = 20;
 const GRID_SIZE_Y = 20;
 const SQUARE_SIZE = 20;
+const DIFFICULTY = 0.1;
 
 interface Square {
 	x: number;
@@ -23,9 +24,9 @@ const createInitialGrid = () => {
 			squares[i][j] = {
 				x: i,
 				y: j,
-				mine: Math.random() < 0.08,
+				mine: Math.random() < DIFFICULTY,
 				guessedMine: false,
-				revealed: true,
+				revealed: false,
 				adjacentMineCount: 0,
 			};
 		}
@@ -60,19 +61,56 @@ const createInitialGrid = () => {
 	return squares;
 };
 
+const fanOut = (grid: Square[][], x: number, y: number) => {
+	if (grid[x][y].mine || grid[x][y].adjacentMineCount > 0) return;
+	const xCheck = [x - 1, x, x + 1].filter(
+		(i) => i !== -1 && i !== GRID_SIZE_X
+	);
+
+	const yCheck = [y - 1, y, y + 1].filter(
+		(j) => j !== -1 && j !== GRID_SIZE_Y
+	);
+
+	const revealed = [];
+
+	for (let i of xCheck) {
+		for (let j of yCheck) {
+			if (!grid[i][j].revealed) {
+				grid[i][j].revealed = true;
+				revealed.push([i, j]);
+			};
+		}
+	}
+
+	// if (revealed.length === 0) return;
+
+	// var nextCoords = revealed[Math.floor(Math.random() * revealed.length)]; // get random new square to fan out from
+	for (let [x, y] of revealed) {
+		if (grid[x][y].adjacentMineCount > 0) continue;
+		fanOut(grid, x, y);
+	}
+
+
+}
+
 const App: React.FC = () => {
 	const [grid, setGrid] = React.useState<Square[][]>(createInitialGrid());
 
 	const handleCellClick: React.MouseEventHandler<HTMLButtonElement> = (e) => {
-		const x = e.currentTarget.getAttribute("data-x") as unknown as number;
-		const y = e.currentTarget.getAttribute("data-y") as unknown as number;
+		const x = Number(e.currentTarget.getAttribute("data-x")) as unknown as number;
+		const y = Number(e.currentTarget.getAttribute("data-y")) as unknown as number;
 		setGrid((prevGrid) => {
 			const gridCpy = [...prevGrid];
+
 			if (x === null || y === null) return prevGrid;
-			if (e.type === "click") {
+
+			if (e.type === "click" && !e.ctrlKey) {
 				if (gridCpy[x][y].mine) alert("dead");
+				fanOut(gridCpy, x, y);
 				gridCpy[x][y].revealed = true;
-			} else {
+			} else if (e.type === "click" && e.ctrlKey) {
+				console.log("ctrl", gridCpy[x][y].guessedMine);
+				gridCpy[x][y].guessedMine = true;
 			}
 			return gridCpy;
 		});
@@ -88,18 +126,30 @@ const App: React.FC = () => {
 	};
 
 	return (
-		<>
+		<AppWrapper>
 			<button onClick={() => setGrid(createInitialGrid())}>reset</button>
 			<GridWrapper>
 				{getGrid().map((x) => (
 					<Square square={x} onClick={handleCellClick} />
 				))}
 			</GridWrapper>
-		</>
+
+		</AppWrapper>
 	);
 };
 
 export default App;
+
+const AppWrapper = styled.main`
+	display: flex;
+	height: 100%;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`
+
+
+
 
 interface SquareProps {
 	square: Square;
@@ -107,41 +157,44 @@ interface SquareProps {
 }
 
 const Square: React.FC<SquareProps> = ({ square, onClick }) => {
-	return (
-		<SquareStyle
-			data-x={square.x}
-			data-y={square.y}
-			onClick={onClick}
-			square={square}
-		>
-			{square.revealed
-				? square.mine
-					? "M"
-					: square.adjacentMineCount > 0
-					? square.adjacentMineCount
-					: ""
-				: ""}
-		</SquareStyle>
-	);
+
+
+	switch (true) {
+		case square.guessedMine: return <BaseSquare data-x={square.x} data-y={square.y} onClick={onClick} x={square.x} y={square.y}>M</BaseSquare>
+		case !square.revealed: return <BaseSquare data-x={square.x} data-y={square.y} onClick={onClick} x={square.x} y={square.y}></BaseSquare>
+		case square.adjacentMineCount > 0: return <ClickedSquare data-x={square.x} data-y={square.y} onClick={onClick} x={square.x} y={square.y}>{square.adjacentMineCount}</ClickedSquare>
+		case square.adjacentMineCount === 0: return <ClickedSquare data-x={square.x} data-y={square.y} onClick={onClick} x={square.x} y={square.y}></ClickedSquare>
+	}
+	return null;
+
 };
 
-const SquareStyle = styled.button<{
-	square: Square;
-}>`
+
+const BaseSquare = styled.button.attrs<{ x: number, y: number }>(props => ({
+	style: {
+		left: props.x * SQUARE_SIZE + "px",
+		top: props.y * SQUARE_SIZE + "px",
+	}
+}))`
 	position: absolute;
-	background: ${({ square }) => (square.revealed ? "#bbb" : "#777")};
-	content: 4;
-	color: black;
+	background: #777;
 	font-size: 10px;
 	height: ${SQUARE_SIZE}px;
 	width: ${SQUARE_SIZE}px;
-	left: ${({ square }) => square.x * SQUARE_SIZE}px;
-	top: ${({ square }) => square.y * SQUARE_SIZE}px;
-	border: 1px ${({ square }) => (square.revealed ? "inlay" : "outlay")} black;
-`;
+	border: 1px solid outlay;
+`
+
+const ClickedSquare = styled(BaseSquare)`
+	background: #bbb;
+	border: 1px solid inlay;
+`
+
+
 
 const GridWrapper = styled.div`
-	/* box-sizing: border-box; */
 	position: relative;
-	border: 2px solid black;
+	height: ${GRID_SIZE_Y * SQUARE_SIZE}px;
+	width: ${GRID_SIZE_X * SQUARE_SIZE}px;
+	border: 3px solid black;
+	/* box-sizing: border-box; */
 `;
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+	html, #root, body {
+		height: 100%;
+	}
+  button {
+    padding: 0;
+  }
+`
+
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GlobalStyles />
   </React.StrictMode>,
   document.getElementById('root')
 )
