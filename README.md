ba069489e14187dc37cb700194c88be6518ff022
Oliver Mansell <Oliver.Mansell@StoneX.com>
2021-12-19 23:00:01 +0000

    chore: remove boilerplate

---
+++ README.md
@@ -0,0 +1,3 @@
+# Minesweeper
+
+I wanted to test Vite, so have made this very quick and very dirty minesweeper implementation. NOT a serious project.
--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const App: React.FC = () => {
 
 	React.useEffect(() => {
 		if (gameState === 'won') {
-			confetti()
+			confetti();
 		}
 	}, [gameState])
 
